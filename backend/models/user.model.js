437fe0e6a68cd6c16f032aa95e0d3b053df36e4d
housssem1999 <62448561+housssem1999@users.noreply.github.com>
2022-06-15 20:23:23 +0100

    backend: authentifcation and crud user

---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+
+    email: String,
+    password: String,
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+      }
+    ],
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      length:8
+    },
+    pic: {
+      type: String,
+      required: false,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  })
+);
+
+module.exports = User;

cc5eb57fef33c0aafd1218e73849a8989849bf75
housssem1999 <62448561+housssem1999@users.noreply.github.com>
2022-06-18 02:40:49 +0100

    bx css importation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,10 @@ import SignUp from './components/pages/SignUp';
 import Login from './components/pages/Login';
 import Layout from './component/layout/Layout';
 import Dashboard from './pages/Dashboard';
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './assets/css/grid.css'
+import './assets/css/theme.css'
+import './assets/css/index.css'
 
 
 
